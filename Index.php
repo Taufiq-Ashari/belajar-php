a546733b01088ed6fafab1ed275f66a180af11e9
Taufiq Ashari <taufiqashari32@gmail.com>
2022-10-30 18:54:35 +0700

    initial commit

---
+++ Index.php
@@ -0,0 +1,33 @@
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link rel="icon" href="aaaa.svg">
+    <title>Banana</title>
+    <?php
+
+    echo "<center> hello Word </center>";
+    
+    $nama = "Taufiq Ashari";
+    $umur = 23;
+
+    echo "Nama saya <strong> $nama</strong>, Saya berusia $umur Tahun<br>";
+    $namakaka = "Ibnu Mubarok";
+    $umurkaka = 28;
+
+    echo "Nama Kaka Saya <strong> $namakaka </strong>, kaka saya berusia $umurkaka tahun<br>";
+
+    $selisihumur = $umurkaka - $umur;
+    echo "selisih usia saya adalah $selisihumur tahun"
+
+    ?>
+</head>
+
+<body>
+
+</body>
+
+</html>
